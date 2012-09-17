667c0bbfc4c1db1ec9611532eeb6e27d11b39132
Rio Akasaka <rio@rioleo.org>
2012-09-17 11:54:10 -0700

    Removed files

--- 4 - Databases/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbHost = "";
-$dbUser = "";
-$dbPass = "";
-$dbDatabase = "";
-$db = mysql_connect("$dbHost", "$dbUser", "$dbPass") or die ("Error connecting to database.");
-$db_found = mysql_select_db("$dbDatabase", $db) or die ("Couldn't select the database.");
-?>
--- 4 - Databases/save.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("config.php");
-
-$result = mysql_query("insert into flicked (img) values ('".$_POST["img"]."');");
-
-?>
